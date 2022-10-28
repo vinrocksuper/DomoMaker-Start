f184449d769334d96b2197f1f2992648a4181c04
Vincent Li <35306435+vinrocksuper@users.noreply.github.com>
2022-10-28 14:15:38 -0400

    finished domomaker B

--- server/router.js
+++ server/router.js
@@ -9,6 +9,7 @@ const router = (app) => {
 
   app.get('/logout', controllers.Account.logout);
   app.get('/maker', controllers.Domo.makerPage);
+  app.post('/maker', controllers.Domo.makeDomo);
   app.get('/', controllers.Account.loginPage);
 };
 
